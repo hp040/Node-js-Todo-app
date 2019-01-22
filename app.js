49155eebf07d457ae3931216512b5e518e64db31
hp040 <patelhardik040@gmail.com>
2019-01-22 21:54:04 +0530

    done

---
+++ app.js
@@ -0,0 +1,30 @@
+var express=require('express');
+var bodyParser=require('body-parser');
+var data=[{task:"get up"}];
+var app=express();
+var port=process.env.port||1337;
+app.set("view engine",'ejs');
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended:false}));
+
+app.use('/assets/',express.static('./assets/'));
+
+app.get('/',function(re,rs,ne){
+    rs.render('todo',{todos: data});
+});
+
+app.post('/create',function(re,rs){
+    
+    data.push(re.body);
+    rs.json(data);
+});
+
+app.delete('/del/:ts',function(re,rs){
+    data=data.filter(function(todo){
+        
+        return todo.task.replace(/ /g, '-') !==re.params.ts;
+    });
+    rs.json(data);
+});
+
+app.listen(port);
---
+++ assets/task.js
@@ -0,0 +1,28 @@
+$(document).ready(function(){
+        
+    $('form').on('submit',function(){      
+       var task_todo=$("form input").val();
+       var send={task:task_todo};
+       
+       $.ajax({type:'POST',
+           url:'/create',
+           data:send,
+           success: function(resp){
+               location.reload();
+           }
+       });
+       return false;
+    });
+
+    $('li').on('click',function(){
+        var data=$(this).text().replace(/ /g,'-');
+        $.ajax({type:'DELETE',
+            url:'/del/'+data,
+            success:function(resp){
+                location.reload();
+            }
+        })
+    });
+    
+});
+
